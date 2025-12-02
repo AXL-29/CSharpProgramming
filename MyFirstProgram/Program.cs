3e6c20514e3def3520cc1a917ac9be01ddd6f17e
AXL-29 <gimpaojade4@gmail.com>
2025-12-02 13:30:25 +0800

    Learn about different escape characters and Console.ReadKey() which waits for a key to press before closing the console window.

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,8 +1,16 @@
-﻿internal class Program
+﻿internal class MainClass
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!"); // Console.WriteLine() is used to print text to the console.
         Console.Beep(); // Console.Beep() produces a beep sound from the console.
+
+        Console.WriteLine();    // Type cw + Tab to generate Console.WriteLine()
+        Console.WriteLine("\t Jade Mark Gimpao");   // Type \t  to add a tab space before the text
+        Console.WriteLine("Ja\bde");    // Type \b to remove the character before it.
+        Console.WriteLine("Ja\nde");    // Type \n to add a new line before the text.
+
+        Console.ReadKey(); // Console.ReadKey() waits for a key press before closing the console window.
+
     }
 }
