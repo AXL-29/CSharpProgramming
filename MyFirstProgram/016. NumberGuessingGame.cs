b8c8239f7737d6430d87e8d177f1da8561888379
Jade Mark Gimpao <gimpaojade4@gmail.com>
2025-12-15 21:47:33 +0800

    Refactor number guessing game logic for improved readability and structure

--- MyFirstProgram/016. NumberGuessingGame.cs
+++ MyFirstProgram/016. NumberGuessingGame.cs
@@ -6,33 +6,54 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            int guess = 0;
+            int guess;
+            int guesses;
+            int min = 1;
+            int max = 100;
+            bool isGameOn = true;
+            String playAgain = "";
 
-            Random random = new Random();
-            int numberToGuess = random.Next(1, 101);
+            Random random = new();
 
-            Console.WriteLine(numberToGuess);
-
-            while (guess != numberToGuess)
+            while (isGameOn)
             {
-                Console.Write("Guess a number between 1 - 100: ");
-                guess = Convert.ToInt32(Console.ReadLine());
+                guess = 0;
+                guesses = 0;
+                int numberToGuess = random.Next(min, max + 1);
+                Console.WriteLine(numberToGuess);
 
-                if (guess > 100 || guess < 0){
-                    Console.WriteLine("Please input a number between 1 - 100 only.");
+                while(guess != numberToGuess)
+                {
+                    Console.Write("Guess a number between " + min + " - " + max + ": ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess < 0 || guess > 100)
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 100 only!");
+                    }
+                    else if (guess < numberToGuess)
+                    {
+                        Console.WriteLine(guess + " is to low!");
+                        guesses++;
+                    }
+                    else if (guess > numberToGuess)
+                    {
+                        Console.WriteLine(guess + " is to high!");
+                        guesses++;
+                    }
                 }
-                else if (guess > numberToGuess){
-                    Console.WriteLine(guess + " is to high!");    
-                }
-                else if (guess < numberToGuess){
-                    Console.WriteLine(guess + " is to low!");    
-                }
-                else{
-                    Console.WriteLine(guess + " is the correct answer, yey!");
+                Console.WriteLine("You have guess the number: " + numberToGuess);
+                Console.WriteLine("Number of guesses: " + guesses);
+
+                Console.Write("Do you want to play again? (y/n): ");
+
+                playAgain = Console.ReadLine().ToLower();
+                if (playAgain != "y")
+                {
+                    Console.WriteLine("Thank you for playing!");
+                    isGameOn = false;
                 }
             }
-
-
             Console.ReadKey();
         }
     }
